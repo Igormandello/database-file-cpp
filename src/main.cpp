21e67f8fc331f7f33dc4366ffb8320008bab89fc
Igor Mandello <igormandello@gmail.com>
2018-10-08 00:34:15 -0300

    Creates a Student structure

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,54 @@
 
 using namespace std;
 
+struct Student {
+  int ra;
+  string name;
+
+  Student(int ra, string name) {
+    this->ra = ra;
+    this->name = name;
+  }
+
+  Student(int ra) {
+    this->ra = ra;
+    this->name = "";
+  }
+
+  Student() {
+    this->ra = 0;
+    this->name = "";
+  }
+
+  void swap(Student& other) {
+    std::swap(this->ra, other.ra);
+    std::swap(this->name, other.name);
+  }
+
+  friend ostream& operator<<(ostream& os, const Student& s) {
+    os << s.ra << " " 
+      << s.name.size() << ":" 
+      << s.name;
+    return os;
+  }
+
+  friend istream& operator>>(istream& is, Student& s) {
+    Student tmp;
+    size_t size;
+    if (is >> tmp.ra >> size) {
+        tmp.name.resize(size);
+
+        is.ignore(1); //Ignore the ':' char
+        if (is.read(&tmp.name[0], size))
+          s.swap(tmp);
+    }
+
+    return is;
+  }
+};
+
 int main() {
+  Database<Student> d ("data.dat", "tree.dat");
+
   return 0;
 }
